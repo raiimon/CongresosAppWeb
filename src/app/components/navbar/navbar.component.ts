34c99aa3e2e6df5fa830ac89ad05af6c08156eac
Borja de Vicente <halfmake@gmail.com>
2019-04-23 12:08:38 +0200

    Añadiendo la foto de perfil del usuario al navbar.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import {Router} from '@angular/router';
+import {UserInterface} from '../../models/user';
 
 @Component({
   selector: 'app-navbar',
@@ -13,13 +14,31 @@ export class NavbarComponent implements OnInit {
   // Variable para comprobar si estamos loggeados.
   public isLogged: boolean = false;
 
+  public provideId: string = 'null';
+
+
   constructor(private authService: AuthService,
               private router: Router,
               private afsAuth: AngularFireAuth) { }
 
+  user: UserInterface = {
+    photoUrl: ''
+  }
+
   ngOnInit() {
     // Llamamos al método nada más se inicie el Navbar.
     this.getCurrentUser();
+
+    this.authService.isAuth().subscribe(user => {
+      if (user) {
+        if (user.photoURL) {
+          this.user.photoUrl = user.photoURL;
+        } else {
+          this.user.photoUrl = '../../assets/img/default-user.png' ;
+        }
+        this.provideId = user.providerData[0].providerId;
+      }
+    });
   }
 
   // Comprobar que el usuario esta autenticado.
