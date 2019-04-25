74dcc1d1df04b48d9dc6e5f031617fbb121bfe98
Borja de Vicente <halfmake@gmail.com>
2019-04-25 13:23:51 +0200

    Añadida la actualización de fotos y refresco de la web.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -30,6 +30,19 @@ export class AuthService {
     });
   }
 
+  updateUser(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const data: UserInterface = {
+      id: user.uid,
+      email: user.email,
+
+      roles: {
+        editor : true
+      }
+    };
+    return userRef.set(data, {merge: true});
+  }
+
   loginEmailUser(email: string, password: string) {
     return new Promise((resolve, reject) => {
       this.afsAuth.auth.signInWithEmailAndPassword(email, password)
