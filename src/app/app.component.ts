7b583e873d330ed060a3f55da47644e23b8c8b2c
Borja de Vicente <halfmake@gmail.com>
2019-04-26 16:10:01 +0200

    Introduciendo el spinner.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {LoadingSpinnersService} from 'ngx-loading-spinners';
+import { LoadingSpinnersService } from 'ngx-loading-spinners';
 
 @Component({
   selector: 'app-root',
