2fcb92b63761d0532ad6b8f5b3b796b5b5091cd3
Borja de Vicente <halfmake@gmail.com>
2019-05-02 13:58:40 +0200

    Eliminando funciones innecesarias

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/auth';
-import {Router} from '@angular/router';
-import {UserInterface} from '../../models/user';
 
 @Component({
   selector: 'app-navbar',
@@ -11,35 +8,7 @@ import {UserInterface} from '../../models/user';
 })
 export class NavbarComponent implements OnInit {
 
-  // Variable para comprobar si estamos loggeados.
-  public isLogged: boolean = false;
+  constructor(private authService: AuthService) { }
 
-  public provideId: string = 'null';
-
-  constructor(private authService: AuthService,
-              private router: Router,
-              private afsAuth: AngularFireAuth) { }
-
-  user: UserInterface = {
-    emailVerified: true,
-    photoUrl: ''
-  };
-
-  public datos = JSON.parse(localStorage.getItem('user'));
-
-  ngOnInit() {
-
-    console.log(this.datos);
-
-    this.authService.isAuth().subscribe(user => {
-      if (user) {
-        if (user.photoURL) {
-          this.user.photoUrl = user.photoURL;
-        } else {
-          this.user.photoUrl = '../../assets/img/default-user.png' ;
-        }
-        this.provideId = user.providerData[0].providerId;
-      }
-    });
-  }
+  ngOnInit() { }
 }
