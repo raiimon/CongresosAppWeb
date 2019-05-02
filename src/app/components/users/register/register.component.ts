59c12aab1439cae8df590a45dc0ec46895834394
Borja de Vicente <halfmake@gmail.com>
2019-05-02 12:30:36 +0200

    Modificando los archivos y realizando la función de verificación de correo.

--- src/app/components/users/register/register.component.ts
+++ src/app/components/users/register/register.component.ts
@@ -1,10 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-// Importamos el servicio y el router.
-import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
-
-// Importamos finalize para luego guardar la ruta de la imagen y recogerla en la base de datos.
-import {LoadingSpinnersService} from 'ngx-loading-spinners';
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -13,59 +8,13 @@ import {LoadingSpinnersService} from 'ngx-loading-spinners';
 })
 export class RegisterComponent implements OnInit {
 
-  // Loader
-  public loaderUniqeName: string;
 
-  constructor(private router: Router,
-              private authService: AuthService,
-              public spinnersService: LoadingSpinnersService) { this.loaderUniqeName = spinnersService.getUniqueName();  }
+  constructor(public authService: AuthService) {
 
-  @ViewChild('imageUser') inputImageUser: ElementRef;
-
-  public email: string = '';
-  public password: string = '';
-
-
-  // Traduccion de los errores de firebase
-  errores: string;
-  erroresEs: string = 'The email address is badly formatted.';
-  vacio: string;
+  }
 
   ngOnInit() {
   }
 
-  // Método para añadir usuario a Firebase.
-  onAddUser() {
-    this.authService.registerUser(this.email, this.password)
-      .then((res) => {
-        this.authService.isAuth().subscribe(user => {
-          if (user) {
-            user.updateProfile({
-              displayName: '',
-              photoURL: ''
-            }).then( () => {
-              this.cargarSpinner();
-            }).catch((error) => {
-            });
-          }
-        });
-      }).catch(error => {
-        // hacemos un if para cuando entre el error en ingles lo traduzca en español
-        if (error.message === this.erroresEs) {
-         this.errores = 'El correo no está bien formateado';
-         this.vacio = this.errores;
-         // con el alert mostramos el error
-         alert(this.vacio);
-        } else {
-          this.errores = 'El correo ya existe'
-          this.vacio = this.errores;
-          alert(this.vacio);
-        }
-        });
-  }
 
-  cargarSpinner() {
-    this.spinnersService.show(this.loaderUniqeName);
-    this.router.navigate(['user/login']);
-  }
 }
