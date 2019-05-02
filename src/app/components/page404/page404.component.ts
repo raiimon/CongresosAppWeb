d148384a253157e443a0cb3194f8c8efa5836912
Borja de Vicente <halfmake@gmail.com>
2019-05-02 15:53:23 +0200

    Arreglando el error del 404.

--- src/app/components/page404/page404.component.ts
+++ src/app/components/page404/page404.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-page404',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page404Component implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  checkLink() {
+      if (this.authService.userData) {
+        this.router.navigate(['']);
+      } else {
+        this.router.navigate(['user/login']);
+      }
+  }
 }
