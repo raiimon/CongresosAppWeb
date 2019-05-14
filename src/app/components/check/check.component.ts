7bf300db3c3767d12c5fab75347636b1103adc18
Borja de Vicente <halfmake@gmail.com>
2019-05-14 15:19:08 +0200

    Añadiendo los datos del control.

--- src/app/components/check/check.component.ts
+++ src/app/components/check/check.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {InvitadoApiService} from '../../services/invitado-api.service';
+import {SalaApiService} from '../../services/sala-api.service';
+import {SalaInterface} from '../../models/sala';
+import {Router} from '@angular/router';
+import {InvitadoInterface} from '../../models/invitado';
 
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
   styleUrls: ['./check.component.css']
 })
-export class CheckComponent {
+export class CheckComponent implements OnInit {
 
-  constructor(private dataApi: InvitadoApiService) {}
+  constructor(private dataApi: InvitadoApiService, private dataRoom: SalaApiService) {}
+
+  // Almacenamos las salas en las que el usuario este.
+  private rooms: SalaInterface[];
+
+  // Variables temporales para crear los valores de las salas.
+  private asistencia: any;
+  private faltas: any;
+
   // Doughnut
   public doughnutChartLabels = ['Asistencia', 'Faltas'];
-  public demodoughnutChartData =  [ 650, 350 ];
+  public demodoughnutChartData: [number, number];
+  public demodoughnutChartDataPrincipal = [670, 230];
 
   // Color del chart
   private donutColors = [
@@ -26,4 +39,22 @@ export class CheckComponent {
   // Tipo de chart.
   public doughnutChartType = 'doughnut';
 
+  ngOnInit() {
+    this.getAllRooms();
+    this.getRandomNumber();
+  }
+
+  getAllRooms() {
+    this.dataRoom.getAllRooms().subscribe(salas => {
+      this.rooms = salas;
+    });
+  }
+
+  // Función temporal que almacena números al azar para los charts
+  getRandomNumber() {
+    this.asistencia = Math.floor(Math.random() * (999 - 100));
+    this.faltas = Math.floor(Math.random() * (425 - 100));
+
+    this.demodoughnutChartData = [ this.asistencia, this.faltas ];
+  }
 }
